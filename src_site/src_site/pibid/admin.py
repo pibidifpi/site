b289de25746182bbc1d0596c122ac3895d1c3f53
charles <cjlmiranda.2@gmail.com>
2015-05-26 10:53:14 -0300

    criação dos módulos de Colaborador, Preco, TipoParticipante, Participante, TipoAtividade, Atividade, Inscricao  no administrador

--- src_site/src_site/pibid/admin.py
+++ src_site/src_site/pibid/admin.py
@@ -1,16 +1,70 @@
 from django.contrib import admin
-from src_site.pibid.models import Empresa, Evento
+from src_site.pibid.models import Empresa, Evento, Colaborador, Preco, TipoParticipante, Participante, TipoAtividade, \
+    Atividade, Inscricao
 
 # Register your models here.
 
 class EmpresaAdmin(admin.ModelAdmin):
-	model = Empresa
-	list_display = ['nome','valor','outros']
-	search_fields = ['nome','valor','outros']
+    model = Empresa
+    list_display = ['nome', 'valor', 'outros']
+    search_fields = ['nome', 'valor', 'outros']
 admin.site.register(Empresa, EmpresaAdmin)
 
+
 class EventoAdmin(admin.ModelAdmin):
-	model = Evento
-	list_display = ['nome','local','dataInicio','dataTermino','resumo']
-	search_fields = ['nome','local','dataInicio','dataTermino','resumo']
+    model = Evento
+    list_display = ['nome', 'local', 'dataInicio', 'dataTermino', 'resumo']
+    search_fields = ['nome', 'local', 'dataInicio', 'dataTermino', 'resumo']
 admin.site.register(Evento, EventoAdmin)
+
+
+class ColaboradorAdmin(admin.ModelAdmin):
+    model = Colaborador
+    list_display = ['nome', 'email', 'informacoes']
+    search_fields = ['nome', 'email', 'informacoes']
+admin.site.register(Colaborador, ColaboradorAdmin)
+
+
+class PrecoAdmin(admin.ModelAdmin):
+    model = Preco
+    list_display = ['normal', 'desconto']
+    search_fields = ['normal', 'desconto']
+admin.site.register(Preco, PrecoAdmin)
+
+
+class TipoParticipanteAdmin(admin.ModelAdmin):
+    model = TipoParticipante
+    list_display = ['nome']
+    search_fields = ['nome']
+admin.site.register(TipoParticipante, TipoParticipanteAdmin)
+
+
+class ParticipanteAdmin(admin.ModelAdmin):
+    model = Participante
+    list_display = ['nome', 'cpf', 'email', 'origem', 'tipo']
+    list_filter = ['tipo__nome']
+    search_fields = ['nome', 'cpf', 'email', 'origem', 'tipo__nome']
+admin.site.register(Participante, ParticipanteAdmin)
+
+
+class TipoAtividadeAdmin(admin.ModelAdmin):
+    model = TipoAtividade
+    list_display = ['nome']
+    search_fields = ['nome']
+admin.site.register(TipoAtividade, TipoAtividadeAdmin)
+
+
+class AtividadeAdmin(admin.ModelAdmin):
+    model = Atividade
+    list_display = ['descricao', 'duracao', 'quantidadeVagas', 'dataInicio', 'dataTermino', 'horario', 'tipo', 'evento']
+    list_filter = ['tipo__nome', 'evento__nome']
+    search_fields = ['descricao', 'duracao', 'quantidadeVagas', 'dataInicio', 'dataTermino', 'horario', 'tipo__nome',
+                 'evento__nome']
+admin.site.register(Atividade, AtividadeAdmin)
+
+
+class InscricaoAdmin(admin.ModelAdmin):
+    model = Inscricao
+    list_display = ['data', 'participante']
+    search_fields = ['data', 'participante__nome']
+admin.site.register(Inscricao, InscricaoAdmin)
--- src_site/src_site/pibid/models.py
+++ src_site/src_site/pibid/models.py
@@ -11,6 +11,7 @@ class Empresa(models.Model):
     def __str__(self):
         return self.nome.encode('utf8')
 
+
 class Evento(models.Model):
     nome = models.CharField(max_length=50, verbose_name='Nome')
     local = models.CharField(max_length=100, verbose_name='Local')
@@ -21,3 +22,66 @@ class Evento(models.Model):
 
     def __str__(self):
         return self.nome.encode('utf8')
+
+
+class Colaborador(models.Model):
+    nome = models.CharField(max_length=50, verbose_name='Nome')
+    email = models.EmailField(verbose_name='Email')
+    informacoes = models.TextField(verbose_name='Informacoes')
+
+    def __str__(self):
+        return self.nome.encode('utf8')
+
+
+class Preco(models.Model):
+    normal = models.FloatField(max_length=50, verbose_name='Normal (R$)')
+    desconto = models.FloatField(verbose_name='Desconto (R$)')
+
+    def __str__(self):
+        return str(self.normal).encode('utf8')
+
+class TipoParticipante(models.Model):
+    nome = models.CharField(max_length=50, verbose_name='Nome')
+
+    def __str__(self):
+        return self.nome.encode('utf8')
+
+class Participante(models.Model):
+    nome = models.CharField(max_length=50, verbose_name='Nome')
+    cpf = models.CharField(max_length=14, verbose_name='CPF')
+    email = models.EmailField(verbose_name='Email')
+    origem = models.CharField(max_length=50, verbose_name='Origem')
+    tipo = models.ForeignKey(TipoParticipante)
+
+    def __str__(self):
+        return self.nome.encode('utf8')
+
+class TipoAtividade(models.Model):
+    nome = models.CharField(max_length=50, verbose_name='Nome')
+
+    def __str__(self):
+        return self.nome.encode('utf8')
+
+class Atividade(models.Model):
+    descricao = models.CharField(max_length=255, verbose_name='Descricao')
+    duracao = models.CharField(max_length=10, verbose_name='Duracao')
+    quantidadeVagas = models.IntegerField(verbose_name='Quantidade de Vagas')
+    dataInicio = models.DateField(verbose_name='Data Inicio')
+    dataTermino = models.DateField(verbose_name='Data Termino')
+    horario = models.CharField(max_length=10, verbose_name='Horario')
+    resumo = models.TextField(verbose_name='Resumo')
+    tipo = models.ForeignKey(TipoAtividade)
+    evento = models.ForeignKey(Evento)
+    colaboradores = models.ManyToManyField(Colaborador)
+    precos = models.ManyToManyField(Preco)
+
+    def __str__(self):
+        return self.descricao.encode('utf8')
+
+class Inscricao(models.Model):
+    data = models.DateTimeField(auto_now=True)
+    participante = models.ForeignKey(Participante)
+    atividades = models.ManyToManyField(Atividade)
+
+    def __str__(self):
+        return str(self.id).encode('utf8')
